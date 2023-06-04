1b4a8c98be6bb692958f10fccebdbd215422b366
AmosDinh <39965380+AmosDinh@users.noreply.github.com>
2023-06-04 20:33:41 +0200

    m

--- 3_remove_tax_images.py
+++ 3_remove_tax_images.py
@@ -7,4 +7,4 @@ for file in os.listdir('data/bicycles_all_keep_nondupes4/'):
 for file in os.listdir('data/bicycles_all_keep_nondupes4/'):
     # remove file if condition is met
     if 'Steuern' in file:
-        os.remove('data/bicycles_all_keep_nondupes4/'+file)
+        os.remove('data/bicycles_all_keep_nondupes4/'+file) 
